42aa0ae17f629cd9fa0b69bd75302b42906af66f
TheWatcher01 <7450@holbertonstudents.com>
2024-04-25 11:13:58 +0200

    Refactor safely_get_value function to use Optional type hint for default parameter

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -8,21 +8,26 @@ Description: Module for getting values from mapping with advanced type
 annotations.
 """
 
-from typing import Mapping, TypeVar, Any, Union
+from typing import Mapping, TypeVar, Any, Union, Optional
 
 T = TypeVar('T')  # Declare type variable
 
 
 def safely_get_value(dct: Mapping, key: Any, default:
-                     Union[T, None] = None) -> Union[Any, T]:
+                     Optional[T] = None) -> Union[Any, T]:
     """
-    Get value from dictionary by key with a default if the key does not exist.
+    Get a value from a dictionary safely.
 
-    Args:
-    dct (Mapping): Dictionary to get the value from.
-    key (Any): Key to look for in the dictionary.
-    default (Union[T, None]): Optional default value to return if key does not
-    exist. Defaults to None.
+    If the key is in the dictionary, return the corresponding value.
+    If the key is not in the dictionary, return the default value.
+
+    Parameters:
+    dct (Mapping): The dictionary to get the value from.
+    key (Any): The key to look for in the dictionary.
+    default (Optional[T]): Default value to return if key is not in dictionary.
+
+    Returns:
+    Union[Any, T]: The value from the dictionary or the default value.
     """
     if key in dct:
         return dct[key]
