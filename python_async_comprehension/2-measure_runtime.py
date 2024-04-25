eb01390c022dab16a3445fbfe3def232a7978773
TheWatcher01 <7450@holbertonstudents.com>
2024-04-25 12:36:35 +0200

    Add return type hint to measure_runtime function in 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -15,7 +15,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Asynchronous function that measure runtime of async_comprehension function
 
