b168694b25415fa8f9eb516e04e7f345e8e42c89
TheWatcher01 <7450@holbertonstudents.com>
2024-04-25 12:34:30 +0200

    Refactor async_comprehension function to use an asynchronous list comprehension in 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
 """
-File: 1-async_comprehension.py
+Module: 1-async_comprehension.py
 Author: TheWatcher01
 Date: 2024-04-24
-Description: Description
+Description: This module contains an asynchronous function that uses an
+asynchronous generator to generate a list of random numbers.
 """
 
-import asyncio
+from typing import List
 
+# Import the async_generator function from the 0-async_generator module
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
-    Asynchronous function that utilizes async generator to create a list of
-    random numbers between 0 and 10.
+    Asynchronously generate a list of random numbers.
 
-    Function creates a list of random numbers between 0 and 10 using an
-    asynchronous generator. It awaits the generator and returns the list.
+    This function uses the async_generator function to asynchronously generate
+    a list of random numbers between 0 and 10. It uses an asynchronous list
+    comprehension to create the list from the generator.
+
+    Returns:
+        List[float]: A list of random numbers between 0 and 10.
     """
-    return [i async for i in async_generator()]
+    # Use an asynchronous list comprehension to create a list from generator
+    return [num async for num in async_generator()]
