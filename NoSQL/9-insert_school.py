66026a23d335f09503004761ee59ae6111ba199b
TheWatcher01 <7450@holbertonstudents.com>
2024-04-29 16:24:55 +0200

    Refactor insert_school function to use insert_one instead of insert in NoSQL/9-insert_school.py and annotations

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -7,8 +7,11 @@ Description: This module contains a function that inserts a new document
 into a MongoDB collection based on kwargs.
 """
 
+from typing import Any
+from pymongo.collection import Collection
 
-def insert_school(mongo_collection, **kwargs):
+
+def insert_school(mongo_collection: Collection, **kwargs: Any) -> Any:
     """
     Inserts a new document into a MongoDB collection based on kwargs.
 
@@ -19,5 +22,5 @@ def insert_school(mongo_collection, **kwargs):
     Returns:
     - The _id of the new document
     """
-    new_doc = mongo_collection.insert(kwargs)
+    new_doc = mongo_collection.insert_one(kwargs)
     return new_doc.inserted_id
