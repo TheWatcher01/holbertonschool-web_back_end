f67f6a6ae29685121e0ff33c95533b5e4ea2f592
TheWatcher01 <7450@holbertonstudents.com>
2024-04-29 17:17:47 +0200

    Add other version of 9-insert_school.py file

---
+++ NoSQL/others_versions/9-insert_school_v1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module: 9-insert_school.py
+Author: TheWatcher01
+Date: 2024-04-29
+Description: This module contains a function that inserts a new document
+into a MongoDB collection based on kwargs.
+"""
+
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document into a MongoDB collection based on kwargs.
+
+    Parameters:
+    - mongo_collection: A pymongo collection object
+    - **kwargs: The data for the new document
+
+    Returns:
+    - The _id of the new document
+    """
+    document = mongo_collection.insert_one(kwargs)
+    return document.inserted_id
