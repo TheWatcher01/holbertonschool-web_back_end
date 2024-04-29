f62a0ae47f11556ce00036f6f9ec90a43aa9b878
TheWatcher01 <7450@holbertonstudents.com>
2024-04-29 17:17:14 +0200

    Add 10-update_topics.py for task 10. Change school topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
-
 """
-File: 10-update_topics.py
+Module: 10-update_topics.py
 Author: TheWatcher01
 Date: 2024-04-29
-Description:
+Description: This module contains a function that changes all topics
+of a school document based on the name
 """
 
-import os
-import sys
-import json
+from typing import List
+from pymongo.collection import Collection
+
+
+def update_topics(mongo_collection:
+                  Collection, name: str, topics: List[str]) -> None:
+    """
+    Changes all topics of a school document based on the name.
+
+    Parameters:
+    - mongo_collection: A pymongo collection object
+    - name: The name of the school to update
+    - topics: The new list of topics
+
+    Returns:
+    - None
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
