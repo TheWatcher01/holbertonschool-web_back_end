128ce738bc3472a3c2dedd7e1f779a5820e60471
TheWatcher01 <7450@holbertonstudents.com>
2024-04-29 16:09:31 +0200

    Adds NoSQL/9-insert_school.py for task 9. Insert a document in Python

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
-
 """
-File: 9-insert_school.py
+Module: 9-insert_school.py
 Author: TheWatcher01
 Date: 2024-04-29
-Description:
+Description: This module contains a function that inserts a new document
+into a MongoDB collection based on kwargs.
 """
 
-import os
-import sys
-import json
+from typing import Any
+from pymongo.collection import Collection
+
+
+def insert_school(mongo_collection: Collection, **kwargs: Any) -> Any:
+    """
+    Inserts a new document into a MongoDB collection based on kwargs.
+
+    Parameters:
+    - mongo_collection: A pymongo collection object
+    - **kwargs: The data for the new document
+
+    Returns:
+    - The _id of the new document
+    """
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
