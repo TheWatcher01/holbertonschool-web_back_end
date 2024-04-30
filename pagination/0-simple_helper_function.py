0a96690132c9dd3a246452fb945c54f56691e6f3
TheWatcher01 <7450@holbertonstudents.com>
2024-04-30 11:14:34 +0200

    Init pagination repo folder with all structure files

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+File: 0-simple_helper_function.py
+Author: TheWatcher01
+Date: 2024-04-30
+Description:
+"""
---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""
+File: 2-hypermedia_pagination.py
+Author: TheWatcher01
+Date: 2024-04-30
+Description:
+"""
