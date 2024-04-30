94dd291c23699e1f0f4aca4d16bae7c4093f2dce
TheWatcher01 <7450@holbertonstudents.com>
2024-04-30 11:59:51 +0200

    Add 0-simple_helper_function.py for task 0. Simple helper function

--- pagination/0-main.py
+++ pagination/0-main.py
@@ -3,12 +3,17 @@
 Main file
 """
 
+# Import the index_range function from the helper module
 index_range = __import__('0-simple_helper_function').index_range
 
+# Get the start and end indices for the first page with 7 items per page
 res = index_range(1, 7)
+# Print the type of the result and the result itself
 print(type(res))
 print(res)
 
+# Get the start and end indices for the third page with 15 items per page
 res = index_range(page=3, page_size=15)
+# Print the type of the result and the result itself
 print(type(res))
 print(res)
--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,5 +4,24 @@
 File: 0-simple_helper_function.py
 Author: TheWatcher01
 Date: 2024-04-30
-Description:
+Description: This module contains a function to assist with pagination.
+It returns a tuple of start and end indices for a given page size.
 """
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Determines the start and end indices for a given page with a specific
+    page size.
+
+    Args:
+    page (int): The current page number, indexed from 1.
+    page_size (int): The number of items per page.
+
+    Returns:
+    tuple: A tuple containing the start and end indices for the items on
+    the page.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
