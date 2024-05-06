915fa1927225fe48d06615a5ed4b14e18ebb36f6
TheWatcher01 <7450@holbertonstudents.com>
2024-05-06 12:22:41 +0200

    fix & lint 0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,41 @@
+/**
+ * @file 0-constants.js
+ * @author TheWatcher01
+ * @date 06-05-2024
+ * @description: This file contains three functions that return strings related to the
+ * usage of 'const' and 'let' in JavaScript.
+ */
+
+/**
+ * @function
+ * @name taskFirst
+ * @description: This function returns a string.
+ * @returns {string}
+ */
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
+/**
+ * @function
+ * @name getLast
+ * @description: This function returns a string.
+ * @returns {string}
+ */
 export function getLast() {
-    return ' is okay';
+  return ' is okay';
 }
 
+/**
+ * @function
+ * @name taskNext
+ * @description: This function returns a string.
+ * @returns {string}
+ */
 export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-    return combination;
+  return combination;
 }
