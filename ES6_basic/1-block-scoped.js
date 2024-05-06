497203bda6277c6f673c98aa433de6ad386e83fa
TheWatcher01 <7450@holbertonstudents.com>
2024-05-06 14:01:13 +0200

    Add comments and fix variable declarations to 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,16 +2,28 @@
  * @file 1-block-scoped.js
  * @author TheWatcher01
  * @date 06-05-2024
- * @description:
+ * @description: Demonstrates block-scoped variables in JavaScript.
  */
 
+/**
+ * Demonstrates block-scoped variables in JavaScript.
+ * Declares two constants, `task` and `task2`, and attempts to redeclare them
+ * within a conditional block. The redeclarations do not affect the initial
+ * declarations due to block scoping. Returns an array of `task` and `task2`.
+ *
+ * @param {boolean} trueOrFalse - Determines if the conditional block is executed.
+ * @returns {Array} An array containing the values of `task` and `task2`.
+ */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    // Attempt to redeclare `task` and `task2` within a block scope.
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
