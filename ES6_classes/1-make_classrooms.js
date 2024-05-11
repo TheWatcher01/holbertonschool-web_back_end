96169e0a9e82ce7ac2fd609400a616bb1d8b07c1
TheWatcher01 <7450@holbertonstudents.com>
2024-05-11 19:04:10 +0200

    Add 1-make_classrooms.js for task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,14 +2,35 @@
  * @file 1-make_classrooms.js
  * @author TheWatcher01
  * @date 11-05-2024
- * @description
+ * @description This file contains the function to initialize classrooms.
  */
 
+import ClassRoom from './0-classroom';
 
+/**
+ * @function initializeRooms
+ * @description Creates and returns an array of ClassRoom instances.
+ * @returns {Array<ClassRoom>} An array of ClassRoom instances.
+ */
 export default function initializeRooms() {
+  /**
+   * @description Create a new ClassRoom instance with a maximum size of 19 students.
+   * @type {ClassRoom}
+   */
   const class1 = new ClassRoom(19);
+
+  /**
+   * @description Create a new ClassRoom instance with a maximum size of 20 students.
+   * @type {ClassRoom}
+   */
   const class2 = new ClassRoom(20);
+
+  /**
+   * @description Create a new ClassRoom instance with a maximum size of 34 students.
+   * @type {ClassRoom}
+   */
   const class3 = new ClassRoom(34);
 
+  // Return an array of the created ClassRoom instances.
   return [class1, class2, class3];
 }
