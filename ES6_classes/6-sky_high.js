7a034be68f185e8e39cc35344de04995936861cd
TheWatcher01 <7450@holbertonstudents.com>
2024-05-12 16:52:02 +0200

    Delete the dot at evacuationWarningMessage in 6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -48,6 +48,6 @@ export default class SkyHighBuilding extends Building {
    * @return {string} The evacuation warning message.
    */
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
