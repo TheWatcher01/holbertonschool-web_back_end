558f8a4391e65cb06bf9710e3ed7eb8a9dbf4a0d
TheWatcher01 <7450@holbertonstudents.com>
2024-05-12 18:18:15 +0200

    Add HolbertonClass with size and location properties

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,49 @@
+/**
+ * @file 8-hbtn_class.js
+ * @author TheWatcher01
+ * @date 12-05-2024
+ * @description Defines the HolbertonClass with size and location properties.
+ */
+
+/**
+ * @class HolbertonClass
+ * @description Represents a HolbertonClass with a size and a location.
+ */
+export default class HolbertonClass {
+  /**
+   * @constructor
+   * @description Constructs a HolbertonClass instance.
+   * @param {number} size - The size of the HolbertonClass.
+   * @param {string} location - The location of the HolbertonClass.
+   * @throws {TypeError} If size is not a number or location is not a string.
+   */
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  /**
+   * @method valueOf
+   * @description Gets the value of the HolbertonClass.
+   * @return {number} The size of the HolbertonClass.
+   */
+  valueOf() {
+    return this._size;
+  }
+
+  /**
+   * @method toString
+   * @description Converts the HolbertonClass object to a string.
+   * @return {string} The location of the HolbertonClass.
+   */
+  toString() {
+    return this._location;
+  }
+}
