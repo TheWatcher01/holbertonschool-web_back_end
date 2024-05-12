be94a39fe35a3dcb7fcff201f44fd9b66f4446bd
TheWatcher01 <7450@holbertonstudents.com>
2024-05-12 15:50:19 +0200

    Refactor Building class to enforce override of evacuationWarningMessage method in  5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,25 +2,44 @@
  * @file 5-building.js
  * @author TheWatcher01
  * @date 12-05-2024
- * @description
+ * @description This module exports a Building class.
  */
 
+/**
+ * @class Building
+ * @description This class represents a building.
+ */
 export default class Building {
+  /**
+   * @constructor
+   * @description Create a building.
+   * @param {number} sqft - The square footage of the building.
+   * @throws {Error} Throws an error if a class extending Building does not override the evacuationWarningMessage method.
+   */
   constructor(sqft) {
     this._sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage
+      === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
+  /**
+   * @getter
+   * @description Get the square footage of the building.
+   * @return {number} The square footage of the building.
+   */
   get sqft() {
     return this._sqft;
   }
 
+  /**
+   * @setter
+   * @description Set the square footage of the building.
+   * @param {number} newSqft - The new square footage of the building.
+   */
   set sqft(newSqft) {
     this._sqft = newSqft;
   }
-
-  /* eslint-disable class-methods-use-this */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-  /* eslint-enable class-methods-use-this */
 }
