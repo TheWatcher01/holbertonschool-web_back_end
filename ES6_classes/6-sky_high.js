e137ed407bb2a236a1abda01534be2c8dc7268b7
TheWatcher01 <7450@holbertonstudents.com>
2024-05-12 16:41:25 +0200

    Add @setter for floor() in 6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -43,6 +43,15 @@ export default class SkyHighBuilding extends Building {
   }
 
   /**
+   * @setter
+   * @description Set the number of floors in the building.
+   * @param {number} newFloors - The new number of floors.
+   */
+  set floors(newFloors) {
+    this._floors = newFloors;
+  }
+
+  /**
    * @method evacuationWarningMessage
    * @description Generate an evacuation warning message for the building.
    * @return {string} The evacuation warning message.
