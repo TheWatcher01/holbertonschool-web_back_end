b02d7f5f569bf127cc29964e6431a546f62e9999
TheWatcher01 <7450@holbertonstudents.com>
2024-05-13 10:57:51 +0200

    Add 0-promise.js & main file for task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -2,6 +2,23 @@
  * @file 0-promise.js
  * @author TheWatcher01
  * @date 13-05-2024
- * @description
+ * @description This module contains a function that returns a new Promise.
  */
 
+/**
+ * @function getResponseFromAPI
+ * @description Returns a new Promise that resolves to 'Request successful'.
+ * @returns {Promise<string>} A Promise that resolves to a string.
+ */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (typeof resolve !== 'function') {
+      throw new TypeError('Resolve must be a function');
+    }
+    if (typeof reject !== 'function') {
+      throw new TypeError('Reject must be a function');
+    }
+
+    resolve('Request successful');
+  });
+}
