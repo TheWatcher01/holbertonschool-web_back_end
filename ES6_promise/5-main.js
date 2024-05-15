9bb88d3a4b80f475432b5f296ddaf7dc981986cc
TheWatcher01 <7450@holbertonstudents.com>
2024-05-15 16:43:00 +0200

    Fix ESLint in the mains files

--- ES6_promise/5-main.js
+++ ES6_promise/5-main.js
@@ -1,3 +1,3 @@
-import uploadPhoto from "./5-photo-reject";
+import uploadPhoto from './5-photo-reject';
 
-console.log(uploadPhoto("guillaume.jpg"));
+console.log(uploadPhoto('guillaume.jpg'));
--- ES6_promise/7-main.js
+++ ES6_promise/7-main.js
@@ -1,19 +1,19 @@
-import loadBalancer from "./7-load_balancer";
+import loadBalancer from './7-load_balancer';
 
-const ukSuccess = "Downloading from UK is faster";
-const frSuccess = "Downloading from FR is faster";
+const ukSuccess = 'Downloading from UK is faster';
+const frSuccess = 'Downloading from FR is faster';
 
-const promiseUK = new Promise(function (resolve, reject) {
+const promiseUK = new Promise(((resolve, reject) => {
   setTimeout(resolve, 100, ukSuccess);
-});
+}));
 
-const promiseUKSlow = new Promise(function (resolve, reject) {
+const promiseUKSlow = new Promise(((resolve, reject) => {
   setTimeout(resolve, 400, ukSuccess);
-});
+}));
 
-const promiseFR = new Promise(function (resolve, reject) {
+const promiseFR = new Promise(((resolve, reject) => {
   setTimeout(resolve, 200, frSuccess);
-});
+}));
 
 const test = async () => {
   console.log(await loadBalancer(promiseUK, promiseFR));
