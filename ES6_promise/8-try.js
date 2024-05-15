e4d7ad13dd625811fb1364660fa8fb2f375a8cce
TheWatcher01 <7450@holbertonstudents.com>
2024-05-15 19:38:44 +0200

    Add 8-try.js & update divideFunction to handle division by zero error task 8. Throw an error

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,6 +1,24 @@
 /**
  * @file 8-try.js
  * @author TheWatcher01
- * @date 13-05-2024
- * @description
+ * @date 15-05-2024
+ * @description The file contains function that divides two numbers.
  */
+
+/**
+ * @function divideFunction
+ * @description divides two numbers & throws an error if denominator is zero.
+ * @param {number} numerator
+ * @param {number} denominator
+ * @throws {Error} cannot divide by 0
+ * @returns {number} result of the division
+ */
+export default function divideFunction(numerator, denominator) {
+  // Check if the denominator is 0
+  if (denominator === 0) {
+    // If it is, throw an error and stop the execution (same as else block)
+    throw new Error('cannot divide by 0');
+  }
+  // Otherwise, return the result of the division
+  return numerator / denominator;
+}
