1610603bf66717c26d50ac06bb6bb6e5f00d505d
TheWatcher01 <7450@holbertonstudents.com>
2024-05-15 17:31:48 +0200

    Add function to simulate photo upload failure in file 5-photo-reject.js for task 5. Reject the promises

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -2,5 +2,18 @@
  * @file 5-photo-reject.js
  * @author TheWatcher01
  * @date 13-05-2024
- * @description
+ * @description Function that simulate photo upload failure by returning rejected Promise.
+ * The Promise is rejected with an Error object that contains a message indicating
+ * the filename cannot be processed.
  */
+
+/**
+ * @function uploadPhoto
+ * @description A function that simulates a photo upload failure.
+ * @param {string} filename - The name of the file to be uploaded
+ * @throws {Error} An Error object with a message indicating the filename cannot be processed
+ * @returns {Promise<never>} A Promise that is always rejected with an Error
+ */
+export default function uploadPhoto(filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
