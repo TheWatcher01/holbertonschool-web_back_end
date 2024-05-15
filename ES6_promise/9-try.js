06ebef7f7c72dbb4b32cc8d0ad026a020757b738
TheWatcher01 <7450@holbertonstudents.com>
2024-05-15 20:10:06 +0200

    Add 9-try.js to implement guardrail function to handle errors in mathFunction execution for task 9. Throw error / try catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -2,5 +2,35 @@
  * @file 9-try.js
  * @author TheWatcher01
  * @date 13-05-2024
- * @description
+ * @description This file contains a function that executes another function and handles errors.
  */
+
+/**
+ * @function guardrail
+ * @description Executes a math function and handles any errors.
+ * @param {Function} mathFunction Function to be executed.
+ * @throws {Error} Any error that occurs during the mathFunction execution.
+ * @returns {Array} An array containing the result or error message, and a guardrail message.
+ */
+export default function guardrail(mathFunction) {
+  // Create an empty queue array to store the result or error message.
+  const queue = [];
+
+  try {
+    // Try to execute the mathFunction and ...
+    const result = mathFunction();
+    // If successful, push the result to the queue.
+    queue.push(result);
+
+    // If an error occurs, catch it and ...
+  } catch (error) {
+    // Push the error message to the queue. (toString() is used to convert error object to string.
+    queue.push(error.toString());
+  }
+
+  // Finally, push the guardrail message to the queue.
+  queue.push('Guardrail was processed');
+
+  // Return the queue array.
+  return queue;
+}
