ee52fff63d89d92609e8ff02272eb3021d494e0e
TheWatcher01 <7450@holbertonstudents.com>
2024-05-21 14:03:59 +0200

    Add updateStudentGradeByCity function to include newGrades parameter in 4-update_grade_by_city.js for task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,5 +5,15 @@
  * @description Module contains function that returns array of students for specific city with
  * their new grade.
  */
-export default function updateStudentGradeByCity() {
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : 'N/A',
+      };
+    });
 }
