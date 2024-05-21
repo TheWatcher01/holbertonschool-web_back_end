ac3cadff4abe40f2d3aaa71e2e718d3752aa9315
TheWatcher01 <7450@holbertonstudents.com>
2024-05-21 15:13:42 +0200

    Implement updateUniqueItems function to handle Map instances in 10-update_uniq_items.js

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -6,5 +6,14 @@
  * their initial quantity.
  */
 
-export default function updateUniqueItems() {
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
 }
