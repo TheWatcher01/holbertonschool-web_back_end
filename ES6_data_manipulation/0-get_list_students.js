6666007eb64a22ce293418480912038326e6a93a
TheWatcher01 <7450@holbertonstudents.com>
2024-05-21 11:24:29 +0200

    Add new students to getListStudents function for task 1

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -6,4 +6,9 @@
  */
 
 export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Colombia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
