03259a2344c851a5d096789c6acdcba919bd6a40
TheWatcher01 <7450@holbertonstudents.com>
2024-05-21 12:49:56 +0200

    Add getListStudentIds function to retrieve student ids from a list in 1-get_list_student_ids.js ofr task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,6 +5,10 @@
  * @description This module contains a function that returns an array of ids from a list of object.
  */
 
-export default function getListStudentIds {
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
+  return students.map((student) => student.id);
 }
