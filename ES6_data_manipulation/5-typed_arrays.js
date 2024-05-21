47fd1881db768d15b24d20e40a47462871192fe3
TheWatcher01 <7450@holbertonstudents.com>
2024-05-21 14:20:34 +0200

    Implement createInt8TypedArray to accept length, position, and value parameters in 5-typed_arrays.js for task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -6,5 +6,15 @@
  * value at specific position.
  */
 
-export default function createInt8TypedArray() {
+export default function createInt8TypedArray(length, position, value) {
+  const arrayBuffer = new ArrayBuffer(length);
+  const dataView = new DataView(arrayBuffer);
+
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  dataView.setInt8(position, value);
+
+  return dataView;
 }
