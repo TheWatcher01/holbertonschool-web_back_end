5e7547fc8d5dcf9872dbb3e30e7ad7a30f5ef2c5
TheWatcher01 <7450@holbertonstudents.com>
2024-05-21 15:31:20 +0200

    Implement queryAPI function to handle endpoint requests for 100-weak.js

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -7,5 +7,14 @@
 
 export const weakMap = new WeakMap();
 
-export default function queryAPI() {
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    if (count >= 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, count + 1);
+  }
 }
