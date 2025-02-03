952500fd4d2ed28aa90a6c77e698bdc126725f7e
TheWatcher01 <7450@holbertonstudents.com>
2025-02-03 09:09:54 +0100

    Add task 6 : a simple http server with express

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,5 +2,17 @@
  * @file 6-http_express.js
  * @author TheWatcher01
  * @date 31-01-2025
- * @description
+ * @description A simple Express server.
  */
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
